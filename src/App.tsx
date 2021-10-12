af7ab9b7f8679cd9070c3457d2d9588342715e6f
Josh Watson <joshua@udel.edu>
2021-10-12 18:39:08 -0400

    Added name

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 UD CIS Scheduler
-                <p>Josh Watson</p>
+                <p>Josh Watson </p>
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
