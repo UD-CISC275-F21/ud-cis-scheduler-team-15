348331f8bbf0860456cb5ca48f147ad4452a493e
Josh Watson <joshua@udel.edu>
2021-10-19 18:22:30 -0400

    fixed line endings

--- src/components/CourseViewer.tsx
+++ src/components/CourseViewer.tsx
@@ -9,30 +9,30 @@ export function CourseViewer({course, dotsStyle, setDotsStyle, setChangePromptVi
     setChangePromptVis: (vis: boolean)=>void;
     }
     
-    ): JSX.Element {
+): JSX.Element {
     return (
         <Table striped={true} bordered hover
-        // Citation: https://stackoverflow.com/questions/61126014/how-to-show-button-while-hover-over-box-using-react
-        onMouseEnter={() => {
-            setDotsStyle({display: "block"});
-        }}
-        onMouseLeave={() => {
-            setDotsStyle({display: "none"});
-        }}>
-        <tbody>
-            <tr>
-                <td>{course.name}</td>
-                <td>{course.number}</td>
-                <td className = "split">
-                    <div className = "credits">{course.credits}</div>
-                    <button className = "dotButton" style = {dotsStyle} onClick = {()=>setChangePromptVis(true)}>
-                        <div className = "dot"></div>
-                        <div className = "dot"></div>
-                        <div className = "dot"></div>
-                    </button>
-                </td>
-            </tr>
-        </tbody>
-    </Table>
+            // Citation: https://stackoverflow.com/questions/61126014/how-to-show-button-while-hover-over-box-using-react
+            onMouseEnter={() => {
+                setDotsStyle({display: "block"});
+            }}
+            onMouseLeave={() => {
+                setDotsStyle({display: "none"});
+            }}>
+            <tbody>
+                <tr>
+                    <td>{course.name}</td>
+                    <td>{course.number}</td>
+                    <td className = "split">
+                        <div className = "credits">{course.credits}</div>
+                        <button className = "dotButton" style = {dotsStyle} onClick = {()=>setChangePromptVis(true)}>
+                            <div className = "dot"></div>
+                            <div className = "dot"></div>
+                            <div className = "dot"></div>
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </Table>
     );
 }
