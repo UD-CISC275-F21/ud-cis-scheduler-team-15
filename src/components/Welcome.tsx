c74529bcfc604f8b15d477da3a7bcb3a01c5cf1d
aerusin <thedarthal@gmail.com>
2021-10-19 18:33:50 -0400

    fixed import

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {Modal} from "react-bootstrap";
 export function Welcome({visible, setVisible}: {visible: boolean, setVisible: (b: boolean) => void}): JSX.Element {
     const hide = () => setVisible(false);
