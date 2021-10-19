5d8da91bb67ffdf88bbfedf1faf49359e26aad12
aerusin <thedarthal@gmail.com>
2021-10-19 18:28:43 -0400

    added welcome modal

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import {Modal, Button} from "react-bootstrap";
+export function Welcome({visible, setVisible}: {visible: boolean, setVisible: (b: boolean) => void}): JSX.Element {
+    const hide = () => setVisible(false);
+    
+    return (
+        <Modal show={visible} onHide = {hide}>
+            <Modal.Header closeButton>
+                <Modal.Title>Welcome!</Modal.Title>
+            </Modal.Header>
+      
+            <Modal.Body>
+                <p>This is a website to help students visualize and plan out there schedule </p>
+            </Modal.Body>
+        </Modal>
+    );
+}
