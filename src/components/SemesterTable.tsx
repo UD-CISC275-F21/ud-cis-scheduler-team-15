c62c76b09428453758a91b1ebc6ebdbdd7f276e3
Josh Watson <joshua@udel.edu>
2021-10-26 14:50:00 -0400

    Added Semester Labels

--- src/components/SemesterTable.tsx
+++ src/components/SemesterTable.tsx
@@ -9,28 +9,38 @@ export function SemesterTable({semester, setSemester}:
     setSemester: (semester: Semester)=>void}):
     JSX.Element{
     return (
-        <Table className = "SemesterTable" striped={true} bordered>
-            <thead>
-                <tr>
-                    <th>
-                        Course Number
-                    </th>
-                    <th>
-                        Course Name
-                    </th>
-                    <th>
-                        Credits
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {semester.courses.map((c:Course, index:number) => { 
-                    return (
-                        <CourseViewer key={index} index={index} semester={semester} setSemester={setSemester}></CourseViewer>
-                    );
-                }
-                )}
-            </tbody>
-        </Table>
+        <div className = "SemesterTable">
+            <table className="SemesterHeader">
+                <td className="yearLabel">
+                    <strong>{semester.year}</strong>
+                </td>
+                <td className="semLabel">
+                    <strong>{semester.semester}</strong>
+                </td>
+            </table>
+            <Table striped={true} bordered>
+                <thead>
+                    <tr>
+                        <th>
+                            Course Number
+                        </th>
+                        <th>
+                            Course Name
+                        </th>
+                        <th>
+                            Credits
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {semester.courses.map((c:Course, index:number) => { 
+                        return (
+                            <CourseViewer key={index} index={index} semester={semester} setSemester={setSemester}></CourseViewer>
+                        );
+                    }
+                    )}
+                </tbody>
+            </Table>
+        </div>
     );
 }
