8254c50be0820a0e6cc7abb5c8ca9d058667532f
Josh Watson <joshua@udel.edu>
2021-10-26 11:37:02 -0400

    Added semester table

--- src/components/SemesterViewer.tsx
+++ src/components/SemesterViewer.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table } from "react-bootstrap";
-// import { CourseViewer } from "./CourseViewer";
+import { Semester } from "../interfaces/semester";
+import { CourseViewer } from "./CourseViewer";
 
-export function SemesterViewer(/*{semester}: {semester: Semester}*/): JSX.Element {
+export function SemesterViewer({semester}: {semester: Semester}): JSX.Element {
+    const [dotsStyle, setDotsStyle] = useState({display: "none"});
+    const [changePromptVis, setChangePromptVis] = useState<boolean>(false);
     return (
         <Table striped={true} bordered hover>
             <thead>
@@ -10,6 +13,9 @@ export function SemesterViewer(/*{semester}: {semester: Semester}*/): JSX.Elemen
                     <td>Name</td>
                     <td>Number</td>
                     <td>Credits</td>
+                </tr>
+                <tr>
+                    <CourseViewer course={semester.courses[0]} dotsStyle={dotsStyle} setDotsStyle={setDotsStyle} setChangePromptVis={setChangePromptVis}></CourseViewer>
                 </tr>
             </thead>
         </Table>
