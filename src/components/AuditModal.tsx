1f7c5f237069592bae973a740b013392140b9441
Josh Watson <joshua@udel.edu>
2021-11-02 13:53:38 -0400

    Only checks rules once (no inf loop)

--- src/components/AuditModal.tsx
+++ src/components/AuditModal.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import {Modal} from "react-bootstrap";
+import React, { useState } from "react";
+import {Modal, Table} from "react-bootstrap";
 import { Semester } from "../interfaces/semester";
+import RequiredCourses from "../assets/RequiredCoreCourses.json";
+import { Course } from "../interfaces/course";
 
 export function AuditModal({plan, visible, setVisible}:
     {plan: Semester[];
@@ -9,14 +11,75 @@ export function AuditModal({plan, visible, setVisible}:
     }): JSX.Element{
     const hide = () => setVisible(false);
 
-    //To appease the linter for now
-    console.log(plan);
+    //Missing Core Courses
+    const [missingCore, setMissingCore] = useState<Course[]>([]);
+    //Only check rules once upon rendering
+    const [checkRules, setCheckRules] = useState<boolean>(true);
+
+    //Get require courses for checking
+    const reqCourses: Course[] = RequiredCourses as Course[];
+    
+    //Raw list of all courses (will be helpful for some checks)
+    const allCourses: Course[] = [];
+    for (let i = 0; i<plan.length; i++){
+        for (let j = 0; j<plan[i].courses.length; j++){
+            allCourses.push(plan[i].courses[j]);
+        }
+    }
+    
+    //Checks for core courses
+    function checkCore():void{
+        const temp_missing: Course[] = [];
+        for(let i = 0; i<reqCourses.length; i++){
+            if(!allCourses.includes(reqCourses[i])){
+                temp_missing.push(reqCourses[i]);
+            }
+            
+        }
+        setMissingCore(temp_missing);
+    }
+
+    //Only do the checks once to avoid inf loop
+    if (checkRules){
+        checkCore();
+        setCheckRules(false);
+    }
 
     return(
         <Modal show={visible} onHide = {hide}>
             <Modal.Header closeButton>
                 <Modal.Title>Schedule Audit</Modal.Title>
             </Modal.Header>
+            <Modal.Body>
+                <div>Core Courses Missing:</div>
+                <Table striped={true} bordered>
+                    <thead>
+                        <tr>
+                            <th>
+                                Course Number
+                            </th>
+                            <th>
+                                Course Name
+                            </th>
+                            <th>
+                                Credits
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {missingCore.map((c:Course, index:number) => { 
+                            return (
+                                <tr key={index}>
+                                    <td className="courseNum">{c.number}</td>
+                                    <td className="courseName">{c.name}</td>
+                                    <td className="credits">{c.credits}</td>
+                                </tr>
+                            );
+                        }
+                        )}
+                    </tbody>
+                </Table>
+            </Modal.Body>
         </Modal>
     );
 }
