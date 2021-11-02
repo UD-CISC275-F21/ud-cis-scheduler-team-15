1a548ecc73725320a7b1875e61f0848939183132
Josh Watson <joshua@udel.edu>
2021-11-02 11:23:02 -0400

    Added aduit button and modal

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,19 @@ import { Welcome } from "./components/Welcome";
 import { PlanViewer } from "./components/PlanViewer";
 import { ControlPanel} from "./components/ControlPanel";
 import { AddSemesterModal } from "./components/AddSemesterModal";
+import { AuditModal } from "./components/AuditModal";
 
 function App(): JSX.Element {
     const [plan, setPlan] = useState<Semester[]>(COURSES as Semester[]);
     const [visible, setVisible] = useState<boolean>(true);
     const [addSemesterModal, showAddSemesterModal] = useState<boolean>(false);
+    const [auditVis, setAuditVis] = useState<boolean>(false);
+
     function addSemester(newSemester: Semester){
         setPlan([...plan, newSemester]);
+    }
+    function showAudit():void{
+        setAuditVis(true);
     }
     function RemoveAllSemesters(){
         setPlan([]);
@@ -25,8 +31,10 @@ function App(): JSX.Element {
             <AddSemesterModal addSemesterModal = {addSemesterModal} showAddSemesterModal = {showAddSemesterModal} addSemester = {addSemester}></AddSemesterModal>
             <ControlPanel showAddSemesterModal = {showAddSemesterModal}></ControlPanel>
             <Button onClick= {RemoveAllSemesters}>Remove All Semesters</Button>
+            <Button onClick= {showAudit}>Audit</Button>
             <PlanViewer plan={plan} setPlan={setPlan}></PlanViewer>
             <Welcome visible = {visible} setVisible = {setVisible}></Welcome>
+            <AuditModal plan={plan} visible={auditVis} setVisible={setAuditVis}></AuditModal>
         </div>
     );
 }
