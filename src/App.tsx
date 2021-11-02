8d40658e92636732a0ee0ee6bb28bd01c8dcbe4f
aerusin <thedarthal@gmail.com>
2021-11-02 13:29:38 -0400

    Changed addsemester to autofill add semesters

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,35 @@ import "./App.css";
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import COURSES from "./assets/courses.json";
-import { Semester } from "./interfaces/semester";
+import { Semester, SemesterType, YearType } from "./interfaces/semester";
 import { Welcome } from "./components/Welcome";
 import { PlanViewer } from "./components/PlanViewer";
-import { ControlPanel} from "./components/ControlPanel";
-import { AddSemesterModal } from "./components/AddSemesterModal";
 
 function App(): JSX.Element {
     const [plan, setPlan] = useState<Semester[]>(COURSES as Semester[]);
     const [visible, setVisible] = useState<boolean>(true);
-    const [addSemesterModal, showAddSemesterModal] = useState<boolean>(false);
-    function addSemester(newSemester: Semester){
+    function addSemester(){
+        console.log(plan.length);
+        let yearType = YearType.FirstYear;
+        let semesterType = SemesterType.Fall;
+        if (plan[plan.length - 1].semester === "Fall") {
+            yearType = plan[plan.length - 1].year;
+            semesterType = SemesterType.Spring;
+            console.log("ADdaddadad");
+        }else{
+            semesterType = SemesterType.Fall;
+            if(plan[plan.length - 1].year === "First Year"){
+                yearType = YearType.SecondYear;
+            }else if(plan[plan.length - 1].year === "Second Year"){
+                yearType = YearType.ThirdYear;
+            }else if(plan[plan.length - 1].year === "Third Year"){
+                yearType = YearType.FourthYear;
+            }
+        }
+        console.log(plan[plan.length - 1].year);
+        console.log(yearType);
+        console.log(semesterType);
+        const newSemester = {year: yearType, semester: semesterType, courses: []};
         setPlan([...plan, newSemester]);
     }
     function RemoveAllSemesters(){
@@ -22,8 +40,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <p>UD CIS Scheduler</p>
-            <AddSemesterModal addSemesterModal = {addSemesterModal} showAddSemesterModal = {showAddSemesterModal} addSemester = {addSemester}></AddSemesterModal>
-            <ControlPanel showAddSemesterModal = {showAddSemesterModal}></ControlPanel>
+            <Button onClick = {addSemester}>Add Semester</Button>
             <Button onClick= {RemoveAllSemesters}>Remove All Semesters</Button>
             <PlanViewer plan={plan} setPlan={setPlan}></PlanViewer>
             <Welcome visible = {visible} setVisible = {setVisible}></Welcome>
