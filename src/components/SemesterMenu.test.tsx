27b66c30c33803ca5f0af249c31beaddf52425ad
Justin <jtenerelli1@gmail.com>
2021-11-17 14:24:27 -0500

    Added more tests for SemesterMenu

--- src/components/SemesterMenu.test.tsx
+++ src/components/SemesterMenu.test.tsx
@@ -1,27 +1,35 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import App from "../App";
 
-describe("Semester Menu", () => { // Runs various tests on SemesterMenu.tsx
+describe("Semester Menu", () => {
+    beforeEach(() => {
+        render(<App />);
+        const button = screen.getByRole('button', { name: "Close" });
+        button.click(); // Closes welcome modal
+    })
 
-    render(<App />); // Renders the app
-    userEvent.click(screen.getByRole('button', { name: "Close" })); // Closes welcome modal
-    const semMenuDots = screen.getAllByLabelText("Sem Menu Dots"); // Stores all buttons that open the Semester Menu in a variable
+    it("is not visible before button is clicked", () => {
+        expect(screen.queryByText(/Add Course to Semester/)).not.toBeVisible;
+        expect(screen.queryByText(/Add a course/)).not.toBeVisible;
+        expect(screen.queryByText(/Delete all courses from semester/)).not.toBeVisible;
+        expect(screen.queryByText(/Delete semester/)).not.toBeVisible;
+    })
 
-    test("is not visible before button is clicked", () => { // Tests that header and buttons are visible
-        expect(screen.getByText(/Add Course to Semester/)).not.toBeVisible;
-        expect(screen.getByText(/Add a course/)).not.toBeVisible;
-        expect(screen.getByText(/Delete all courses from semester/)).not.toBeVisible;
-        expect(screen.getByText(/Delete semester/)).not.toBeVisible;
-    });
-
-    userEvent.click(semMenuDots[0]); // Clicks on the first Semester Menu button (corresponds to First Year, Fall)
-
-    test("is visible after button is clicked", () => { // Tests that header and buttons are visible
+    it("is visible after button is clicked", () => {
+        screen.getAllByLabelText("Sem Menu Dots")[0].click(); // Clicks on the first Semester Menu button (corresponds to First Year, Fall)
         expect(screen.getByText(/Add Course to Semester/)).toBeVisible;
         expect(screen.getByText(/Add a course/)).toBeVisible;
         expect(screen.getByText(/Delete all courses from semester/)).toBeVisible;
         expect(screen.getByText(/Delete semester/)).toBeVisible;
-    });
-});
+    })
+
+    it("is not visible after menu is closed", () => {
+        screen.getAllByLabelText("Sem Menu Dots")[0].click();
+        screen.getByLabelText("Close").click(); // Close Semester Menu modal
+        expect(screen.queryByText(/Add Course to Semester/)).not.toBeVisible;
+        expect(screen.queryByText(/Add a course/)).not.toBeVisible;
+        expect(screen.queryByText(/Delete all courses from semester/)).not.toBeVisible;
+        expect(screen.queryByText(/Delete semester/)).not.toBeVisible;
+    })
+})
