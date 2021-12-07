a465cabb238178f15937cf55c2318d0a51826076
Josh Watson <joshua@udel.edu>
2021-12-07 11:30:35 -0500

    Fixing line endings

--- src/components/AuditTable.tsx
+++ src/components/AuditTable.tsx
@@ -1,1 +1,36 @@
 import React from "react";
+import { Table } from "react-bootstrap";
+import { Course } from "../interfaces/course";
+
+export function AuditTable({courses}:
+    {courses: Course[]}): JSX.Element{
+    return(
+        <Table className="AuditTable" striped={true} bordered>
+            <thead>
+                <tr>
+                    <th className="text-center">
+                        Course Number
+                    </th>
+                    <th className="text-center">
+                        Course Name
+                    </th>
+                    <th className="text-center">
+                        Credits
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {courses.map((c:Course, index:number) => { 
+                    return (
+                        <tr key={index}>
+                            <td className="text-center">{c.number}</td>
+                            <td className="text-center">{c.name}</td>
+                            <td className="text-center">{c.credits}</td>
+                        </tr>
+                    );
+                }
+                )}
+            </tbody>
+        </Table>
+    );
+}
