59778ac00a321455cd383a45043bae2677a7a098
Josh Watson <joshua@udel.edu>
2021-12-07 11:58:24 -0500

    Deleting last course does not crash prog

--- src/components/CourseViewer.tsx
+++ src/components/CourseViewer.tsx
@@ -19,45 +19,50 @@ export function CourseViewer({index, sem_index, plan, setPlan, setRenderSemester
         setRenderSemester(true);
         setRenderCourse(false);
     }
-    
-    return (
-        <>
-            <td className="courseNum" aria-label="Course Number"
-                onMouseEnter={() => {
-                    setDotsStyle({display: "block"});
-                }}
-                onMouseLeave={() => {
-                    setDotsStyle({display: "none"});
-                }}>{
-                    plan[sem_index].courses[index].number
-                }
-            </td>
-            <td className="courseName" aria-label="Course Name"
-                onMouseEnter={() => {
-                    setDotsStyle({display: "block"});
-                }}
-                onMouseLeave={() => {
-                    setDotsStyle({display: "none"});
-                }}>{
-                    plan[sem_index].courses[index].name
-                }
-            </td>
-            <td className = "split" aria-label="Credits"
-                // Citation: https://stackoverflow.com/questions/61126014/how-to-show-button-while-hover-over-box-using-react
-                onMouseEnter={() => {
-                    setDotsStyle({display: "block"});
-                }}
-                onMouseLeave={() => {
-                    setDotsStyle({display: "none"});
-                }}>
-                <div className = "credits">{plan[sem_index].courses[index].credits}</div>
-                <button className = "dotButton" data-testid = "dots" style = {dotsStyle} onClick = {()=>setChangePromptVis(true)}>
-                    <div className = "dot"></div>
-                    <div className = "dot"></div>
-                    <div className = "dot"></div>
-                </button>
-            </td>
-            <ChangeData index={index} sem_index={sem_index} plan={plan} setPlan={setPlan} visible={changePromptVis} setVisible={setChangePromptVis} setRenderSemester={setRenderCourse}></ChangeData>
-        </>
-    );
+    if (plan[sem_index].courses[index]){
+        return (
+            <>
+                <td className="courseNum" aria-label="Course Number"
+                    onMouseEnter={() => {
+                        setDotsStyle({display: "block"});
+                    }}
+                    onMouseLeave={() => {
+                        setDotsStyle({display: "none"});
+                    }}>{
+                        plan[sem_index].courses[index].number
+                    }
+                </td>
+                <td className="courseName" aria-label="Course Name"
+                    onMouseEnter={() => {
+                        setDotsStyle({display: "block"});
+                    }}
+                    onMouseLeave={() => {
+                        setDotsStyle({display: "none"});
+                    }}>{
+                        plan[sem_index].courses[index].name
+                    }
+                </td>
+                <td className = "split" aria-label="Credits"
+                    // Citation: https://stackoverflow.com/questions/61126014/how-to-show-button-while-hover-over-box-using-react
+                    onMouseEnter={() => {
+                        setDotsStyle({display: "block"});
+                    }}
+                    onMouseLeave={() => {
+                        setDotsStyle({display: "none"});
+                    }}>
+                    <div className = "credits">{plan[sem_index].courses[index].credits}</div>
+                    <button className = "dotButton" data-testid = "dots" style = {dotsStyle} onClick = {()=>setChangePromptVis(true)}>
+                        <div className = "dot"></div>
+                        <div className = "dot"></div>
+                        <div className = "dot"></div>
+                    </button>
+                </td>
+                <ChangeData index={index} sem_index={sem_index} plan={plan} setPlan={setPlan} visible={changePromptVis} setVisible={setChangePromptVis} setRenderSemester={setRenderCourse}></ChangeData>
+            </>
+        );
+    }else{
+        return(
+            <></>
+        );
+    }
 }
