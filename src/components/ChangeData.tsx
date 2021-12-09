7abbba0c6145c81019969a9690fce743eb98cd01
jtenerelli017 <41691080+jtenerelli017@users.noreply.github.com>
2021-12-09 00:49:05 -0500

    Reworked to use React model for user input

--- src/components/ChangeData.tsx
+++ src/components/ChangeData.tsx
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { useState } from "react";
 import {Modal} from "react-bootstrap";
 //import ReactDOM, { render } from "react-dom";
 //import App from "../App";
@@ -16,6 +16,10 @@ export function ChangeData({index, sem_index, plan, setPlan, visible, setVisible
     setRenderSemester: (renderSemester: boolean)=>void;
     }): JSX.Element{
 
+    const [courseNumber, setCourseNumber] = useState<string>(plan[sem_index].courses[index].number);
+    const [courseName, setCourseName] = useState<string>(plan[sem_index].courses[index].name);
+    const [courseCredits, setCourseCredits] = useState<number>(plan[sem_index].courses[index].credits);
+
     function setCourse(c: Course):void{
         const temp_sem: Semester = plan[sem_index];
         plan[sem_index].courses[index] = c;
@@ -27,10 +31,7 @@ export function ChangeData({index, sem_index, plan, setPlan, visible, setVisible
     const hide = () => setVisible(false);
 
     function update():void{
-        const numberHTML = document.getElementById("numberUpdate") as HTMLInputElement;
-        const nameHTML  = document.getElementById("nameUpdate") as HTMLInputElement;
-        const creditsHTML = document.getElementById("creditsUpdate") as HTMLInputElement;
-        setCourse({number:numberHTML.value, name: nameHTML.value, credits: Number(creditsHTML.value)});
+        setCourse({number: courseNumber, name: courseName, credits: courseCredits});
         hide();
     }
 
@@ -63,16 +64,20 @@ export function ChangeData({index, sem_index, plan, setPlan, visible, setVisible
             <Modal.Body>
                 <div className="dataEditorLabel">
                     <strong>Edit Course Number:</strong>
-                    <input className="dataInput" id="numberUpdate" data-testid="numberUpdate" type="text" defaultValue={plan[sem_index].courses[index].number}></input>
+                    <input className="dataInput" id="numberUpdate" data-testid="numberUpdate" type="text" defaultValue={plan[sem_index].courses[index].number} onChange={(e) => setCourseNumber(e.target.value)}></input>
                 </div>
                 <div className="dataEditorLabel">
                     <strong>Edit Course Name:</strong>
-                    <input className="dataInput" id="nameUpdate" data-testid="nameUpdate" defaultValue={plan[sem_index].courses[index].name}></input>
+                    <input className="dataInput" id="nameUpdate" data-testid="nameUpdate" defaultValue={plan[sem_index].courses[index].name} onChange={(e) => setCourseName(e.target.value)}></input>
                 </div>
                 <div className="dataEditorLabel">
                     <strong>Edit Credits:</strong>
                     <div>
-                        <input className="dataInputCred" id="creditsUpdate" data-testid="creditsUpdate" type="number" defaultValue={plan[sem_index].courses[index].credits}></input>
+                        {/* credit:
+                        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+                        https://github.com/UD-CISC275-F21/ta-trainer/blob/e7e3538c946ac04cd9a6b225ef2b93270ecf32af/src/components/AddCardModal.tsx
+                        */}
+                        <input className="dataInputCred" id="creditsUpdate" data-testid="creditsUpdate" type="number" defaultValue={plan[sem_index].courses[index].credits} onChange={(e) => setCourseCredits(parseInt(e.target.value))}></input>
                     </div>
                 </div>
                 <button className="saveChanges" data-testid = "saveCourseChanges" onClick={() => update()}>Save Changes</button>
